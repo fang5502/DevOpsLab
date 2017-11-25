a3625df421ee4e4c14ef521b4ca105b275aee744
fang5502 <fang5502@gmail.com>
2017-11-25 15:10:56 +0800

    add func div

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"net/http"
 	"strconv"
 
@@ -29,9 +30,34 @@ func main() {
 		return c.String(http.StatusOK, strconv.Itoa(result))
 	})
 
+	e.GET("/div/:num1/:num2", func(c echo.Context) error {
+		num1Str := c.Param("num1")
+		num2Str := c.Param("num2")
+		num1, err := strconv.Atoi(num1Str)
+		if err != nil {
+			return c.String(http.StatusUnprocessableEntity, err.Error())
+		}
+		num2, err := strconv.Atoi(num2Str)
+		if err != nil {
+			return c.String(http.StatusUnprocessableEntity, err.Error())
+		}
+		result, err := Div(num1, num2)
+		if err != nil {
+			return c.String(http.StatusUnprocessableEntity, err.Error())
+		}
+		return c.String(http.StatusOK, strconv.Itoa(result))
+	})
+
 	e.Logger.Fatal(e.Start(":1323"))
 }
 
 func Add(num1, num2 int) int {
 	return num1 + num2
+}
+
+func Div(num1, num2 int) (int, error) {
+	if num2 == 0 {
+		return 0, errors.New("number 2 cnannot be 0")
+	}
+	return num1 / num2, nil
 }
